d0e34e8382144de09bde67528a0f4e7a3fb1e5d0
Luke Schuyler <lukeschuyler1@gmail.com>
2017-03-08 13:25:57 -0600

    thorugh query 13

--- chinook-queries.sql
+++ chinook-queries.sql
@@ -25,3 +25,51 @@ FROM  Invoice
 SELECT *
 FROM Invoice i, Customer c
 WHERE c.Country = 'Brazil'
+
+-- 7.
+SELECT i.*, e.FirstName AS 'Employee FirstName', e.LastName AS 'Employee LastName'
+FROM Invoice i 
+LEFT JOIN Employee e
+WHERE e.Title = 'Sales Support Agent'
+
+-- 8. ??? YALKED TO JOE, KINDA VAGUE
+SELECT i.Total, 
+c.FirstName AS 'Cust FirstName', 
+c.LastName AS 'Cust LastName', 
+i.BillingCountry, 
+e.FirstName AS 'Emp FirstName', 
+e.LastName AS 'Emp LastName'
+FROM Invoice i 
+LEFT JOIN Customer c 
+LEFT JOIN Employee e
+
+-- 9.
+SELECT InvoiceDate, COUNT(*), SUM(Total)
+FROM Invoice
+WHERE substr (InvoiceDate,1, 4) = '2009' 
+OR substr (InvoiceDate,1, 4) = '2011'
+GROUP BY (substr (InvoiceDate,1, 4))
+
+-- 10.
+SELECT InvoiceId, COUNT(InvoiceId)
+FROM InvoiceLine
+WHERE InvoiceId = '37'
+
+-- 11.
+SELECT InvoiceId, COUNT(InvoiceId)
+FROM InvoiceLine
+GROUP BY InvoiceId
+
+-- 12.
+SELECT i.*, t.Name AS 'Track Name'
+FROM InvoiceLine i, Track t
+ON i.TrackId = t.TrackId
+
+-- 13.
+SELECT i.*, t.Name AS 'Track Name', ar.Name AS 'Artist Name'
+FROM InvoiceLine i, Track t, Artist ar, Album al
+ON i.TrackId = t.TrackId 
+AND al.ArtistId = ar.ArtistId 
+AND al.AlbumId = t.AlbumId
+
+-- 14.
